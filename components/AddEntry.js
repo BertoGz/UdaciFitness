a7170528382aa71a1d994969635a28cac450592c
BertoGz <AlbertoG@albertos-mbp.attlocal.net>
2020-06-17 23:55:15 -0700

    added methods to AddEntry

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,10 +1,20 @@
 import React,{Component} from 'react'
-import {View} from 'react-native'
-import {getMetricMetaInfo} from '../utils/helpers'
+import {View, TouchableOpacity, Text} from 'react-native'
+import {getMetricMetaInfo, timeToString} from '../utils/helpers'
 
 import UdaciSlider from './UdaciSlider'
 import UdaciStepper from './UdaciStepper'
 import DateHeader from './DateHeader'
+
+
+function SubmitBtn({onPress}){
+	return(
+		<TouchableOpacity onPress={onPress}>
+			<Text>Submit</Text>
+		</TouchableOpacity>
+	)
+}
+
 export default class AddEntry extends Component{
 
 	state={
@@ -49,38 +59,61 @@ export default class AddEntry extends Component{
 
 	}
 
+	submit = () =>{
+		const key = timeToString()
+		const entry = this.state
+
+		//Update redux
+
+		this.setState(()=>({
+		run:0,
+		bike:0,
+		swim:0,
+		sleep:0,
+		eat:0,  
+		}))
+
+		//navigate to home
+
+		//save to db
+
+		//clear local notification
+	}
+
 	render(){
 		const metaInfo = getMetricMetaInfo()
 		return(
 			<View>
 				<DateHeader date={(new Date()).toLocaleDateString()}/>
-
-				{Object.keys(metaInfo).map((item)=>{
-					const {getIcon,type,...rest} = metaInfo[item]
-					const value = this.state[item] // makes the value = to the state[metric]
+				{Object.keys(metaInfo).map((key)=>{
+					const {getIcon,type,...rest} = metaInfo[key]
+					const value = this.state[key] // makes the value = to the state[metric]
 
 					return(
-						<View key={item}>
+						<View key={key}>
 							{getIcon()}
 							{
 								type === 'slider' ?
 								<UdaciSlider
 									value={value}
-									onChange={(value)=>this.slide(metric,value)}
+									onChange={(value)=>this.slide(key,value)}
 									{...rest}
 								/>  :
 
 								<UdaciStepper 
 									value={value}
-									onIncrement={()=>this.increment(metric)}
-									onDecrement={()=>this.decrement(metric)}
+									onIncrement={()=>this.increment(key)}
+									onDecrement={()=>this.decrement(key)}
 									{...rest}
 								/>
 							}
 						</View>
 					)
 				})}
+				<SubmitBtn onPress={this.submit}/>
 			</View>
+
+			
 		)
 	}
 }
