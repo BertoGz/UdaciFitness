5f66dbeb6b8020e5d0e1db3e45c0441e80693195
BertoGz <AlbertoG@albertos-mbp.attlocal.net>
2020-06-18 17:03:47 -0700

    added styling for metric of no Data

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import{createStore} from 'redux'
 import {Provider} from 'react-redux'
 import reducer from './reducers'
 
+import History from './components/History'
+
 export default class App extends React.Component {
 
 
@@ -15,7 +17,9 @@ export default class App extends React.Component {
       return (
       	<Provider store={createStore(reducer)}>
 	        <View style={{flex:1}}>
-	          <AddEntry/>
+	        	<View style={{height:20}}/>
+	          <History/>
+	          
 	        </View>
 	    </Provider>
       );
